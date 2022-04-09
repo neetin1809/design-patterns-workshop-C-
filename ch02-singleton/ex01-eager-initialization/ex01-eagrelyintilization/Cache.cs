05d47af6351edceb071be32f1259839e6eacf379
Nitin Vaswani <nitin.vaswani@rediffmail.com>
2022-04-10 01:43:19 +0530

    addede eagrely intilization

---
+++ ch02-singleton/ex01-eager-initialization/ex01-eagrelyintilization/Cache.cs
@@ -0,0 +1,15 @@
+﻿namespace ex01_eagrelyintilization
+{        public sealed class Cache
+        {
+            private static Cache cache = new Cache();
+
+            private Cache()
+            {
+
+            }
+
+            public static Cache getInstance() => cache;
+        }
+    }
+
+
